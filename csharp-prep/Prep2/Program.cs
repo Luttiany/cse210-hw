b424613b26d1fc23163900c915ab6ee6869e2f37
Luttiany Dias <luttiany@gmail.com>
2024-06-23 00:32:33 -0300

    prep 1 and 2 ready to go

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Welcome! Please inform your grade percentage:");
+
+        string grade = Console.ReadLine ();
+        int percentage = int.Parse (grade);
+
+        
+
+        if (percentage >= 90)
+        {
+            Console.WriteLine ("Your grade is A");
+        }
+        else if (percentage >= 80)
+        {
+            Console.WriteLine ("Your grade is B");
+        }
+        else if (percentage >= 70)
+        {
+            Console.WriteLine ("Your grade is C");
+        }
+        else if (percentage >= 60)
+        {
+            Console.WriteLine ("Your grade is D");
+        }
+        else
+        {
+            Console.WriteLine ("Your grade is F");
+        }
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine ("Congratulations! You passed!");
+        }
+        else
+        {
+            Console.WriteLine ("Better luck next time, try again!");
+        }
+
     }
 }
