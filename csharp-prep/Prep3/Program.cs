6c4f8993f265dbd8fdf666638a3d27ed3d8e022c
Luttiany Dias <luttiany@gmail.com>
2024-06-25 09:37:14 -0300

    atualização

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Welcome to the 'Guess The Number' game!");
+        Random randomGen = new Random ();
+        int number = randomGen.Next (1, 101);
+        int guessNumber = 0;
+
+        while (number != guessNumber)
+
+        {
+            Console.WriteLine ("Please, guess a number from 1 to 100: ");
+            guessNumber = int.Parse(Console.ReadLine());
+            
+            if (number > guessNumber)
+            {
+                Console.WriteLine ("The number is higher!");
+            }
+
+            else if (number < guessNumber)
+            {
+                Console.WriteLine ("The number is lower!");
+            }
+
+            else
+            {
+                Console.WriteLine ("You guessed the number!!");
+
+            }
+            
+        }
+
     }
 }
